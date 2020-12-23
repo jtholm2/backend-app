c482e3ed48937bc374ce35d99e95f79f280f6bc6
JT Holmes <jtholmes@microsoft.com>
2020-12-23 20:18:49 +0000

    Updated server.js

--- dist/server.js
+++ dist/server.js
@@ -12,23 +12,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-//const { request } = require('express');
-//const { BlobServiceClient, ContainerClient, BlockBlobClient, BlobClient } = require('@azure/storage-blob');
 //import BlobServiceClient from '@azure/storage-blob';
 //import Connection from 'tedious';
 //import Request from 'tedious';
-//const fs = require('fs');
+//import { SSL_OP_EPHEMERAL_RSA } from 'constants';
 const fs_1 = __importDefault(require("fs"));
-//const express = require('express');
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-//const path = require('path');
 const path_1 = __importDefault(require("path"));
-//const fetch = require("node-fetch");
 const node_fetch_1 = __importDefault(require("node-fetch"));
-//const { SSL_OP_EPHEMERAL_RSA } = require('constants');
-//import { SSL_OP_EPHEMERAL_RSA } from 'constants';
-const port = process.env.PORT;
+const port = 3000;
 app.use(express_1.default.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -87,8 +80,8 @@ app.post('/azurestorage', (req, res) => __awaiter(void 0, void 0, void 0, functi
     if (fileName === 'blank') {
         res.json({ "html": { "h1": "<h1>You need to select a file from the drop down.</h1>" } });
     }
-    else if (fs_1.default.existsSync(path_1.join(__dirname,'..', 'storedfiles', `${fileName}`))) {
-        const rawdata = fs_1.default.readFileSync(path_1.join(__dirname,'..', 'storedfiles', `${fileName}`));
+    else if (fs_1.default.existsSync(path_1.default.join(__dirname, '..', 'storedfiles', `${fileName}`))) {
+        const rawdata = fs_1.default.readFileSync(path_1.default.join(__dirname, '..', 'storedfiles', `${fileName}`));
         const testData = JSON.parse(rawdata.toString());
         console.log("data is about to send\n");
         res.json(testData);
@@ -107,7 +100,7 @@ app.post('/azurestorage', (req, res) => __awaiter(void 0, void 0, void 0, functi
     // }
 }));
 app.get('/kassiendpoint', (req, res) => {
-    res.sendFile(path_1.join(__dirname,'..', 'public', 'Kassi.html'));
+    res.sendFile(path_1.default.join(__dirname, '..', 'public', 'Kassi.html'));
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
